ea97895cc20cab9e0022622c798fee6810593909
lykmapipo <lallyelias87@gmail.com>
2019-01-30 18:15:28 +0300

    refactor: improve example seeds

--- examples/seeds/features/index.js
+++ examples/seeds/features/index.js
@@ -2,12 +2,11 @@
 
 
 /* dependencies */
-const zones = require('./zones');
 const regions = require('./regions');
+const districts = require('./districts');
+const wards = require('./wards');
 const warehouses = require('./warehouses');
 
-module.exports = exports = (
-  [].concat(zones)
-  .concat(regions)
-  .concat(warehouses)
-);
+module.exports = exports = [
+  ...regions, ...districts, ...wards, ...warehouses
+];
