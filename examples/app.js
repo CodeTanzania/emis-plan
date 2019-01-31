46cf5b3f2e98fa353c1deb24ba80437c52e0b7c2
lykmapipo <lallyelias87@gmail.com>
2019-01-31 19:53:12 +0300

    refactor: improve example seeds

--- examples/app.js
+++ examples/app.js
@@ -1,82 +1,40 @@
 'use strict';
 
-// TODO refactor and use real seeds
-
-/* ensure mongodb uri */
-process.env.MONGODB_URI =
-  (process.env.MONGODB_URI || 'mongodb://localhost/emis-plan');
-
 
 /* dependencies */
-const path = require('path');
-const _ = require('lodash');
-const async = require('async');
-const mongoose = require('mongoose');
 const app = require('@lykmapipo/express-common');
+const { connect } = require('@lykmapipo/mongoose-common');
 const { include } = require('@lykmapipo/include');
 const { jsonSchema } = require('@lykmapipo/mongoose-common');
-const { Permission, permissionRouter } = require('@lykmapipo/permission');
-const { Feature, featureRouter } = require('@codetanzania/emis-feature');
-const { Role, roleRouter } = require('@codetanzania/emis-role');
-const { Party, partyRouter } = require('@codetanzania/emis-stakeholder');
+const { permissionRouter } = require('@lykmapipo/permission');
+const { featureRouter } = require('@codetanzania/emis-feature');
+const { roleRouter } = require('@codetanzania/emis-role');
+const { partyRouter } = require('@codetanzania/emis-stakeholder');
+const { alertSourceRouter, alertRouter } = require('@codetanzania/emis-alert');
+const { incidentTypeRouter } = require('@codetanzania/emis-incident-type');
 const {
-  AlertSource,
-  Alert,
-  alertSourceRouter,
-  alertRouter
-} = require('@codetanzania/emis-alert');
-const {
-  Warehouse,
-  Item,
-  Stock,
-  Adjustment,
   warehouseRouter,
   itemRouter,
   stockRouter,
   adjustmentRouter
 } = require('@codetanzania/emis-resource');
 const {
-  Indicator,
-  Question,
-  Questionnaire,
   indicatorRouter,
   questionRouter,
   questionnaireRouter
 } = require('@codetanzania/emis-questionnaire');
 const {
-  IncidentType,
-  incidentTypeRouter
-} = require('@codetanzania/emis-incident-type');
-const {
-  Incident,
-  Action,
-  Task,
   incidentRouter,
   actionRouter,
   taskRouter
 } = require('@codetanzania/emis-incident');
 const {
-  Plan,
   planRouter,
-  Activity,
   activityRouter,
-  Procedure,
   procedureRouter,
   apiVersion,
   info
-} = require(path.join(__dirname, '..'));
-
-/* establish mongodb connection */
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
-
-
-/* refs */
-let features;
-let warehouses;
-let parties;
-let roles;
-let items;
-let incidentTypes;
+} = include(__dirname, '..');
 
 
 /* mount routers */
@@ -101,259 +59,28 @@ app.mount(incidentRouter);
 app.mount(actionRouter);
 app.mount(taskRouter);
 
-/* seed and start */
-function boot() {
 
-  const startedAt = new Date();
-  console.log('Seed Start', startedAt);
+// establish mongodb connection
+connect(error => {
+  // re-throw if error
+  if (error) { throw error; }
 
-  async.waterfall([
-    function clearQuestionnaires(next) {
-      Questionnaire.deleteMany(function ( /*error, results*/ ) {
-        next();
-      });
-    },
-
-    function clearQuestions(next) {
-      Question.deleteMany(function ( /*error, results*/ ) {
-        next();
-      });
-    },
-
-    function clearIndicators(next) {
-      Indicator.deleteMany(function ( /*error, results*/ ) {
-        next();
-      });
-    },
-
-    function seedIndicators(next) {
-      const indicators = Indicator.fake(5);
-      Indicator.seed(indicators, next);
-    },
-
-    function seedQuestions(indicators, next) {
-      let questions = Question.fake(indicators.length);
-      questions = _.map(questions, function (question, index) {
-        questions[index].indicator = indicators[index];
-        return question;
-      });
-      Question.seed(questions, next);
-    },
-
-    function seedQuestionnaires(questions, next) {
-      const questionnaire = Questionnaire.fake();
-      questionnaire.questions = [...questions];
-      Questionnaire.seed(questionnaire, function ( /*error, results*/ ) {
-        next();
-      });
-    },
-
-    function seedPermissions(next) {
-      Permission.seed(function ( /*error, results*/ ) {
-        next();
-      });
-    },
-
-    function seedFeatures(next) {
-      Feature.seed(function (error, results) {
-        features = results;
-        warehouses =
-          _.filter(features, feature => feature.type === 'Warehouse');
-        next();
-      });
-    },
-
-    function seedRoles(next) {
-      Role.seed(function (error, results) {
-        roles = results;
-        next();
-      });
-    },
-
-    function seedParties(next) {
-      Party.seed(function (error, results) {
-        parties = results;
-        next();
-      });
-    },
-
-    function seedAlertSources(next) {
-      AlertSource.seed(function ( /*error, results*/ ) {
-        next();
-      });
-    },
-
-    function seedAlerts(next) {
-      Alert.seed(function ( /*error, results*/ ) {
-        next();
-      });
-    },
-
-    function seedItems(next) {
-      Item.seed(function (error, results) {
-        items = results;
-        next();
-      });
-    },
-
-    function seedStocks(next) {
-      const stocks = _.map(items, (item, index) => {
-        return {
-          store: warehouses[index % warehouses.length],
-          owner: parties[index % parties.length],
-          item: item,
-          quantity: Math.ceil(Math.random() * 1000),
-          minAllowed: Math.ceil(Math.random() * 10),
-          maxAllowed: Math.ceil(Math.random() * 10000),
-        };
-      });
-      Stock.seed(stocks, next);
-    },
-
-    function seedAdjustment(stocks, next) {
-      const adjustments = _.map(stocks, (stock) => {
-        const adjustment = Adjustment.fake();
-        adjustment.item = stock.item;
-        adjustment.stock = stock;
-        adjustment.store = stock.store;
-        adjustment.party = stock.owner;
-        adjustment.quantity = Math.ceil(Math.random() * 100);
-        adjustment.cost = Math.ceil(Math.random() * 10000);
-        return adjustment;
-      });
-      Adjustment.insertMany(adjustments, ( /*error , stocks*/ ) => next());
-    },
-
-    function clearProcedures(next) {
-      Procedure.deleteMany(function ( /*error, results*/ ) {
-        next();
-      });
-    },
-
-    function clearActivities(next) {
-      Activity.deleteMany(function ( /*error, results*/ ) {
-        next();
-      });
-    },
-
-    function clearPlans(next) {
-      Plan.deleteMany(function ( /*error, results*/ ) {
-        next();
-      });
-    },
-
-    function seedIncidentTypes(next) {
-      IncidentType.seed((error, results) => {
-        incidentTypes = results;
-        next(error, results);
-      });
-    },
-
-    function seedPlans(incidentTypes, next) {
-      const plans = _.map(incidentTypes, function (incidentType, index) {
-        return {
-          owner: _.find(parties, { abbreviation: 'DARMAERT' }),
-          incidentType: incidentType,
-          boundary: _.find(features, { name: 'Dar-es-salaam' })
-        };
-      });
-      Plan.insertMany(plans, next);
-    },
-
-    function seedActivities(plans, next) {
-      let activities = include(__dirname, 'seeds', 'activities');
-      activities = _.map(plans, function (plan, index) {
-        return _.map(activities, function (activity) {
-          activity.plan = plan;
-          activity.primary = _.sampleSize(roles, 1);
-          activity.supportive = _.sampleSize(roles, 2);
-          return activity;
-        });
-      });
-      activities = _.flattenDeep(activities);
-      Activity.insertMany(activities, next);
-    },
-
-    function seedProcedures(activities, next) {
-      const procedures = Procedure.fake(activities.length);
-      _.forEach(activities, function (activity, index) {
-        procedures[index].activity = activity;
-        procedures[index].number = (index % 2) + 1;
-        procedures[index].primary = _.sampleSize(roles, 1);
-        procedures[index].supportive = _.sampleSize(roles, 2);
-      });
-      Procedure.insertMany(procedures, (error) => next(error));
-    },
-
-    function clearTasks(next) {
-      Task.deleteMany(function ( /*error, results*/ ) {
-        next();
-      });
-    },
-
-    function clearActions(next) {
-      Action.deleteMany(function ( /*error, results*/ ) {
-        next();
-      });
-    },
-
-    function clearIncidents(next) {
-      Incident.deleteMany(function ( /*error, results*/ ) {
-        next();
-      });
-    },
-
-    function seedIncidents(next) {
-      const incidents = Incident.fake(incidentTypes.length);
-      _.forEach(incidentTypes, function (incidentType, index) {
-        incidents[index].type = incidentType;
-      });
-      Incident.insertMany(incidents, next);
-    },
-
-    function seedActions(incidents, next) {
-      const actions = Action.fake(incidents.length);
-      _.forEach(incidents, function (incident, index) {
-        actions[index].incident = incident;
-      });
-      Action.insertMany(actions, next);
-    },
-
-    function seedTasks(actions, next) {
-      const tasks = Task.fake(actions.length);
-      _.forEach(actions, function (action, index) {
-        tasks[index].action = action;
-        tasks[index].number = (index % 2) + 1;
-      });
-      Task.insertMany(tasks, next);
-    }
-
-  ], function (error, results) {
-    console.log(error);
-    const endedAt = new Date();
-    console.log('Seed End', endedAt);
-    console.log('Seed Time', endedAt.getTime() - startedAt.getTime());
-
-    /* expose module info */
-    app.get('/', function (request, response) {
-      response.status(200);
-      response.json(info);
-    });
-
-    /* expose api schemas */
-    app.get(`/v${apiVersion}/schemas`, function (request, response) {
-      const schema = jsonSchema();
-      response.status(200);
-      response.json(schema);
-    });
-
-    /* fire the app */
-    app.start(function (error, env) {
-      console.log(`visit http://0.0.0.0:${env.PORT}`);
-    });
-
+  // expose module info
+  app.get('/', (request, response) => {
+    response.status(200);
+    response.json(info);
   });
 
-}
+  // expose api schemas
+  app.get(`/v${apiVersion}/schemas`, function (request, response) {
+    const schema = jsonSchema();
+    response.status(200);
+    response.json(schema);
+  });
 
-boot();
+  // fire the app
+  app.start((error, env) => {
+    console.log(`visit http://0.0.0.0:${env.PORT}`);
+  });
+
+});
